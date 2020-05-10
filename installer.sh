baae077146f2a3c4527246cd4f81add91c581e5c
crabarca <caabarca1@uc.cl>
2020-05-09 23:39:03 -0400

    Added installer script with basic symlinking

---
+++ installer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+## 
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Links for dev folder
+ln -s ${BASEDIR}/dev/.vimrc ~/.vimrc
+ln -s ${BASEDIR}/dev/.tmux.conf ~/.tmux.conf
+
+# Links for shell folder
+ln -s ${BASEDIR}/shell/.zshrc ~/.zshrc
+
