2fba9f06c83664f7c54d7f6b1fcd6d481eeaeaab
Cristobal Abarca <cristobal.abarca95@gmail.com>
2024-03-06 20:28:31 +0100

    Add installation script

--- installer.sh
+++ installer.sh
@@ -9,9 +9,17 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Links for dev folder
-ln -sfn ${BASEDIR}/dev/.vimrc ~/.vimrc
-ln -sfn ${BASEDIR}/dev/.tmux.conf ~/.tmux.conf
+ln -sfn ${BASEDIR}/dev/vimrc ~/.vimrc
+ln -sfn ${BASEDIR}/dev/tmux.conf ~/.tmux.conf
+
+
+# Check if oh-my-zsh is already installed
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  # Install oh-my-zsh
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
 # Links for shell folder
-ln -sfn ${BASEDIR}/shell/.zshrc ~/.zshrc
+ln -sfn ${BASEDIR}/shell/zshrc ~/.zshrc
+
 
